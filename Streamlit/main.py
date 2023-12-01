683492ce88e0f7c727a3117a3c598600fdfb8aa8
NP <63650218+nikhilpolpakkara@users.noreply.github.com>
2023-12-01 17:21:34 +0530

    db_update
    date and date_time updated

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,20 +1,19 @@
 import streamlit as st
 from pymongo import MongoClient
 
+import datetime
 import Mongodb_querries
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["TNV"]
-
+line_color = "#2a9df4"
 
 
 def tyre_wear_page(department, test_activity, department_details):
 
-    st.title(test_activity)
-    #
-    # # Example: Save button
-    # if st.button("Save"):
-    #     st.success(f"Data saved for {department} - {test_activity}")
+    st.title(f":blue[{test_activity.upper()}]")
+
+
     tyre_wear_doc = Mongodb_querries.get_document_from_nested_array(
         collection=department_details,
         collection_filter={'name': department},
@@ -29,7 +28,7 @@ def tyre_wear_page(department, test_activity, department_details):
             sub_class = entry["sub_class"]
             if sub_class not in expander_list.keys():
                 expander_list[sub_class] = {
-                    "expander": st.expander(sub_class),
+                    "expander": st.expander(sub_class.upper()),
                     "variables": []
                 }
 
@@ -37,55 +36,62 @@ def tyre_wear_page(department, test_activity, department_details):
                 {"variable_name": entry["name"], "st_input_type": entry["st_input_type"], "hide": entry["hide"],
                  "required": entry["required"]})
 
-
     # print(expander_list)
-    def create_expander():
-        selected_value = st.session_state['tyre_location']
-        if selected_value:
-            with st.expander(selected_value):
-                st.write(f"{selected_value} entry")
+
     for sub_class in expander_list.keys():
         for variable in expander_list[sub_class]["variables"]:
             label_extension = " *" if (variable["required"] == 1) else ""
             if not variable["hide"] == 1:
-                if variable["st_input_type"] == "selectbox":
-
-                    # expander_list[sub_class]["expander"].selectbox(variable["variable_name"] + label_extension,
-                    #                                                options=["Front", "Rear"],
-                    #                                                key = variable["variable_name"],
-                    #                                                placeholder="Select default method..."
-                    #                                                )
-                    pass
+                if variable['variable_name'] == "date":
+                    expander_list[sub_class]["expander"].date_input("SELECT DATE", datetime.datetime.now().date(), key="date")
                 else:
-                    expander_list[sub_class]["expander"].text_input(variable["variable_name"] + label_extension,
+                    expander_list[sub_class]["expander"].text_input((variable["variable_name"] + label_extension).upper(),
                                                                     key=variable["variable_name"])
 
-    with st.container(border=True):
+
+    st.markdown(f'<hr style="border-top: 1px solid {line_color};">', unsafe_allow_html=True)
+
+    with st.container():
         for variable_dict in tyre_wear_doc["test_data"]:
             if "dependant_variables" in variable_dict:
                 for option in variable_dict["options"]:
-                    with st.expander(option + " entry"):
+                    with st.expander((option + " entry").upper()):
                         for entry in variable_dict["dependant_variables"]:
-                            st.text_input(entry, key=f"{option}_{entry}")
+                            st.text_input(entry.upper(), key=f"{option}_{entry}")
 
     # print(st.session_state)
 
+    st.markdown(f'<hr style="border-top: 1px solid {line_color};">', unsafe_allow_html=True)
+
     tyre_wear_collection = db["tyre_wear"]
-    if st.button("Save Document"):
+    if st.button(("Save").upper(), use_container_width=True):
         for entry in tyre_wear_doc["test_data"]:
-            if (entry['required'] == 1) & (st.session_state[entry['name']] == ""):
-                save_to_db = False
-                st.error(f"Please enter {entry['name']} field")
-                break
-            else:
-                save_to_db = True
+            try:
+                if (entry['required'] == 1) & (st.session_state[entry['name']] == ""):
+                    save_to_db = False
+                    st.error(f"Please enter {entry['name']} field")
+                    break
+                else:
+                    save_to_db = True
+            except:
+                pass
         if save_to_db:
-            save_to_mongodb("tyre_wear", st.session_state)
+            # print("---------------------------------------\n\n\n\n")
+            # # print(st.session_state)
+            # print(dict(st.session_state))
+            # print("---------------------------------------\n\n\n\n")
+            # print(type(dict(st.session_state)))
+            # print("---------------------------------------\n\n\n\n")
+            document_data = dict(st.session_state)
+            print("---------------------------------------")
+            document_data["date"] = document_data['date'].strftime('%d-%m-%Y')
+            document_data["date_time"] = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+            save_to_mongodb("tyre_wear", document_data)
 
 
 
 def emission_page(department, test_activity, department_details):
-    st.title(test_activity)
+    st.title(test_activity.upper())
 
 def save_to_mongodb(collection_name, document_data):
     # Insert the document into the MongoDB collection
@@ -102,17 +108,19 @@ def main():
     department_details = db["department_details"]
 
 
-    st.sidebar.title("Select Department and Test Activity")
+    st.sidebar.title(":blue[SELECT DEPARTMENT & TEST ACTIVITY]")
+
+    st.sidebar.markdown(f'<hr style="border-top: 1px solid {line_color};">', unsafe_allow_html=True)
 
     dept_names = Mongodb_querries.get_field_values_from_collection(collection= department_details, field_name='name')
 
-    selected_department = st.sidebar.selectbox("Select Department", dept_names)
+    selected_department = st.sidebar.selectbox(("Select Department").upper(), dept_names)
 
     activity_list = Mongodb_querries.get_field_values_from_nested_array(collection=department_details,
                                                                         collection_field_name='test_activity',
                                                                         array_field_name='name',
                                                                         filter={'name': selected_department})
-    selected_test_activity = st.sidebar.selectbox("Select Test Activity", activity_list)
+    selected_test_activity = st.sidebar.selectbox(("Select Test Activity").upper(), activity_list)
 
     # Display the data entry page based on the selected department and test activity
     if selected_test_activity == 'tyre_wear':
