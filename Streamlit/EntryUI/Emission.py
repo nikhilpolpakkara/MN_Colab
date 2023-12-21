3c0a41b0b8d7906b0d73b2b8e1ea445091b0c2d5
NP <63650218+nikhilpolpakkara@users.noreply.github.com>
2023-12-21 11:09:01 +0530

    major_update_3

--- Streamlit/EntryUI/Emission.py
+++ Streamlit/EntryUI/Emission.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from Misc import get_html_report
 from DBOps import crud_operations
+import pandas as pd
+from pymongo import MongoClient
 
 def emission_entry():
     pass
@@ -33,10 +35,12 @@ def emission_analysis_ui():
 
 
 def emission_dashboard_2w():
-    client =
+    client = MongoClient("mongodb://localhost:27017/")
     st.title("EMISSION DASHBOARD")
-    bosch_tab = st.tabs("BOSCH")
+    bosch_tab, vdpl_tab = st.tabs(["BOSCH", "VDPL"])
     datahandler = crud_operations.MongoDBHandler(client)
+    datahandler.load_database("testing_history")
+    datahandler.load_collection("emission_dashboard")
     with bosch_tab:
         pipeline = [
             {
@@ -55,26 +59,35 @@ def emission_dashboard_2w():
             }
         ]
 
-        result = list(collection.aggregate(pipeline))
-
-        # Prepare data for plotting
-        data = {"model": [], "pass_count": [], "fail_count": []}
-
+        result = list(datahandler.collection.aggregate(pipeline))
+        data = {"model": [], "result": [], "count": []}
         for item in result:
-            model = item["_id"]["model"]
-            count = item["count"]
-
-            if item["_id"]["result"] == "PASS":
-                data["pass_count"].append(count)
-                data["fail_count"].append(0)  # Add 0 for fail count to align with pass counts
-            elif item["_id"]["result"] == "FAIL":
-                data["fail_count"][-1] = count  # Update the last fail count
-
-            if model not in data["model"]:
-                data["model"].append(model)
+            data["model"].append(item["_id"]["model"])
+            data["result"].append(item["_id"]["result"])
+            data["count"].append(item["count"])
+        df = pd.DataFrame(data)
+        df = df.sort_values(by="model")
+        # # Prepare data for plotting
+        # data = {"model": [], "pass_count": [], "fail_count": []}
+        #
+        # for item in result:
+        #     model = item["_id"]["model"]
+        #     count = item["count"]
+        #
+        #
+        #     if item["_id"]["result"] == "PASS":
+        #         data["pass_count"].append(count)
+        #         data["fail_count"].append(0)  # Add 0 for fail count to align with pass counts
+        #     elif item["_id"]["result"] == "FAIL":
+        #         data["fail_count"][-1] = count  # Update the last fail count
+        #
+        #     if model not in data["model"]:
+        #         data["model"].append(model)
 
         # Create a DataFrame
-        df = pd.DataFrame(data)
+        # df = pd.DataFrame(data)
+        print("OK")
+
     vdpl_tab = st.tabs("VDPL")
 
 
---
+++ Streamlit/rough.py
@@ -0,0 +1,3 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://nikhilpolpakkara:Aspire_13@cluster0.4cun9lz.mongodb.net/?retryWrites=true&w=majority")
